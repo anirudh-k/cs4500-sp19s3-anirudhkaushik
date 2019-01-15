8403c549728f29f26aaaf36416f336b3421485b8
anirudh-k <anirudh.kaushik0@gmail.com>
2019-01-15 09:15:12 -0500

    Initial commit/starter files

---
+++ src/main/java/edu/neu/cs4500/cs4500sp19s3CCIS/DemoApplication.java
@@ -0,0 +1,14 @@
+package edu.neu.cs4500.cs4500sp19s3CCIS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/edu/neu/cs4500/cs4500sp19s3CCIS/DemoApplicationTests.java
@@ -0,0 +1,17 @@
+package edu.neu.cs4500.cs4500sp19s3CCIS;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
