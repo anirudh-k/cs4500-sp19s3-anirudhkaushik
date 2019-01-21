8b54b163e12e3132890e53056ef22b4386db1ca7
anirudh-k <anirudh.kaushik0@gmail.com>
2019-01-21 11:50:51 -0500

    create basic UserService

---
+++ src/main/java/edu/neu/cs4500/services/UserService.java
@@ -0,0 +1,13 @@
+package edu.neu.cs4500.services;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.neu.cs4500.model.User;
+
+@RestController
+public class UserService {
+  static List<User> users = new ArrayList<>();
+}
