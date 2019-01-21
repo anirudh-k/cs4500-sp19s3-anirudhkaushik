0fbc51d12875dd53154be750e398e849b235d428
anirudh-k <anirudh.kaushik0@gmail.com>
2019-01-21 11:54:04 -0500

    expose individual user

--- src/main/java/edu/neu/cs4500/services/UserService.java
+++ src/main/java/edu/neu/cs4500/services/UserService.java
@@ -1,6 +1,7 @@
 package edu.neu.cs4500.services;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -15,5 +16,15 @@ public class UserService {
   @GetMapping("/api/user")
   public List<User> findAllUsers(){
     return users;
+  }
+
+  @GetMapping("api/user/{userId}")
+  public User findUserById(@PathVariable("userId") Integer id) {
+    for (User u : users) {
+      if (u.getId() == id) {
+        return u;
+      }
+    }
+    return null;
   }
 }
